e2beddbafab339d95be4f47936faab54532194b6
kd536 <35612185+kd536@users.noreply.github.com>
2018-03-30 22:45:26 -0400

    Add files via upload

---
+++ Problem 2.py
@@ -0,0 +1,5 @@
+print('If the probability of 5-year survival is q then the number of participants \
+      who survived beyond 5 years in a cohort of N participants would follow binomial distribution.')
+print('The expected value of X would be N x q.')
+print('The variance of X would be N x q(1-q).')
+
